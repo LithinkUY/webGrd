c9e04e14a29843f29ec479c51a71c83aebca400e
LithinkUY <info.lithink@gmail.com>
2026-03-01 22:21:03 -0300

    fix: remove webpack config incompatible with Turbopack

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,6 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
-  // Excluir carpetas que no son parte del proyecto Next.js
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/nueva demo/**', '**/www.cdrmedios.com/**', '**/node_modules/**'],
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
